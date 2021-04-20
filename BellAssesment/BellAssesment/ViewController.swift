53b963b0883b1e83c1f468521f849dbbb90ce910
Rana ijaz ahmad <ijaz0066@hotmail.com>
2021-04-20 02:04:35 -0400

    initial commit

---
+++ BellAssesment/BellAssesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BellAssesment
+//
+//  Created by ijaz ahmad on 2021-04-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
